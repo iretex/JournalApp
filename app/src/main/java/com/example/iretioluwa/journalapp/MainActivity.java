a97a9746921688ba138ace92dfc9938b734d50ab
iretex <iretioluwa.olawuyi@outlook.com>
2018-06-30 12:21:18 +0100

    Journal app with login functionality.

--- app/src/main/java/com/example/iretioluwa/journalapp/MainActivity.java
+++ app/src/main/java/com/example/iretioluwa/journalapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.iretioluwa.journalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
